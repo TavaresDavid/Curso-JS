57996655036430692ce6853d9c67c5e9d2bd5c82
David Gomes <132388029+TavaresDavid@users.noreply.github.com>
2023-10-30 20:41:23 -0300

    criei o ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while(c <= 6) {
+    console.log(` ${c} passo dado`)
+    c++ // c = c+1
+}
+
+/*var c = 6
+do {
+    console.log(` ${c} passo dado`)
+    c++ // c = c+1
+} while(c <= 6) */
